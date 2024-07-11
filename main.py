9187ec62ef526e4449717e9f305342405f0684ec
Akanksha Muthe <akankshamuthe@gmail.com>
2024-07-11 12:27:47 +0530

    CSV file upload

---
+++ main.py
@@ -0,0 +1,35 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+data = pd.read_csv("LoanApprovalPrediction.csv")
+
+data.head(5)
+
+obj = (data.dtypes == 'object')
+print("Categorical variables:", len(list(obj[obj].index)))
+
+data.drop(['Loan_ID'], axis=1, inplace=True)
+
+obj = (data.dtypes == 'object')
+object_cols = list(obj[obj].index)
+plt.figure(figsize=(18,36))
+index = 1
+
+for col in object_cols:
+    y = data[col].value_counts()
+    plt.subplot(11,4,index)
+    plt.xticks(rotation=90)
+    sns.barplot(x=list(y.index), y=y)
+    index +=1
+
+from sklearn import preprocessing
+
+label_encoder = preprocessing.LabelEncoder()
+obj = (data.dtypes == 'object')
+for col in list(obj[obj].index):
+    data[col] = label_encoder.fit_transform(data[col])
+
+obj = (data.dtypes == 'object')
+print("Categorical variables:", len(list(obj[obj].index)))
